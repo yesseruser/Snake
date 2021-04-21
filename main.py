8a79b5e29905d9ec2b7acd9f5b92b497c8ce0648
yesseruser <matej2.petricek@outlook.cz>
2021-04-21 15:47:57 +0200

    Added classes

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+import random
+
+class snake(object):
+    def __init__(self):
+        pass
+    def get_head_position(self):
+        pass
+    def turn(self):
+        pass
+    def move(self):
+        pass
+    def reset(self):
+        pass
+    def draw(self):
+        pass
+    def handle_keys(self):
+        pass
+class food(object):
+    def __init__(self):
+        pass
+    def randomize_position(self):
+        pass
+    def draw(self, surface):
+        pass
